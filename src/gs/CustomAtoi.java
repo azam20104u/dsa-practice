eacc61a49a08478242d26961774d4d920561bb3d
Azam Amir Reza <azam20104u@gmail.com>
2026-03-11 01:47:11 +0530

    feat: adding Goldman Sachs questions

---
+++ src/gs/CustomAtoi.java
@@ -0,0 +1,24 @@
+package gs;
+
+public class CustomAtoi {
+    public static int customAtoi(String s) {
+        int i = 0;
+        int num = 0;
+        while (i<s.length()) {
+            if (Character.isDigit( s.charAt(i))){
+                while (i<s.length() && Character.isDigit( s.charAt(i))){
+                    num = num*10 + (s.charAt(i)-'0');
+                    i++;
+                }
+                break;
+            }
+            i++;
+        }
+        return num;
+    }
+    public static void main(String[] args) {
+        System.out.println(customAtoi("-234246asdd3")); // -234246
+        System.out.println(customAtoi("abc123xyz"));    // 123
+        System.out.println(customAtoi("abc"));          // 0
+    }
+}
