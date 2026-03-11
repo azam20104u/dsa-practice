ba03c1b808ec1c7b2d3200c279b56395b4d024cd
Azam Amir Reza <azam20104u@gmail.com>
2026-03-11 13:59:21 +0530

    GS: MaximumPossibleSum

---
+++ src/gs/MaximumPossibleSum.java
@@ -0,0 +1,22 @@
+package gs;
+
+public class MaximumPossibleSum {
+    static int maxSum(int[] arr, int k){
+        int n = arr.length;
+        int curSum = 0;
+        for (int i = 0; i<k; i++){
+            curSum+=arr[i];
+        }
+        int max = curSum;
+        for (int i = 1; i<=k; i++){
+            curSum = curSum - arr[k-i] + arr[n-i];
+            max = Math.max(max, curSum);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSum(new int[]{1,2,3,4,5},2));
+        System.out.println(maxSum(new int[]{2,2,2},2));
+    }
+}
